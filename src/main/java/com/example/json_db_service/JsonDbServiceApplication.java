6280df257979e0d7f9a8108cce9fc4490f539f89
Andrey Pletenev <pletenev.andrey@yandex.ru>
2022-08-08 12:18:46 +0700

    Initial spring setup

---
+++ src/main/java/com/example/json_db_service/JsonDbServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.json_db_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonDbServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsonDbServiceApplication.class, args);
+    }
+
+}
