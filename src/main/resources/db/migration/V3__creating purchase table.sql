d3ce34c105263840cc4e9bf4ba018a09af1814f6
Andrey Pletenev <pletenev.andrey@yandex.ru>
2022-08-11 17:59:44 +0700

    Исправлено создание таблицы purchase

--- src/main/resources/db/migration/V3__creating purchase table.sql
+++ src/main/resources/db/migration/V3__creating purchase table.sql
@@ -1,12 +1,9 @@
 create table purchase
 (
-    id   bigserial
-        constraint purchase_pk
-            primary key
-        constraint purchase_customer_id_fk
-            references customer
-        constraint purchase_product_id_fk
-            references product,
-    date date not null
+    id bigserial constraint purchase_pk primary key,
+    customer_id bigserial not null,
+    product_id bigserial not null,
+    date date NOT NULL,
+    foreign key (customer_id) references customer (id),
+    foreign key (product_id) references product (id)
 );
-
