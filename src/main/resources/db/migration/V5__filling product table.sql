e81fc4962b565d378e6cb1a4dbb5678b9152767b
Andrey Pletenev <pletenev.andrey@yandex.ru>
2022-08-11 17:23:01 +0700

    Заполнена таблица product

---
+++ src/main/resources/db/migration/V5__filling product table.sql
@@ -0,0 +1,63 @@
+INSERT INTO public.product (id, name, price)
+VALUES (DEFAULT, 'ПАКЕТ БОЛЬШОЙ БИО', 690);
+
+INSERT INTO public.product (id, name, price)
+VALUES (DEFAULT, 'ТЕСТО ХЛЕБНЫЙ ДОМ ЗВЕЗДНОЕ СЛОЕН', 11490);
+
+INSERT INTO public.product (id, name, price)
+VALUES (DEFAULT, 'СОДА ПИЩЕВАЯ 500Г', 3450);
+
+INSERT INTO public.product (id, name, price)
+VALUES (DEFAULT, 'КАПУСТА БЕЛОКОЧАННАЯ', 8490);
+
+INSERT INTO public.product (id, name, price)
+VALUES (DEFAULT, 'АПЕЛЬСИНЫ ФАС.', 16990);
+
+INSERT INTO public.product (id, name, price)
+VALUES (DEFAULT, 'СЫР МОЦАРЕЛЛА ДЛЯ ПИЦЦЫ 250Г', 19190);
+
+INSERT INTO public.product (id, name, price)
+VALUES (DEFAULT, 'СЫР ЛЕГЕНДА АЛТАЯ МЯГКИЙ 45%', 53000);
+
+INSERT INTO public.product (id, name, price)
+VALUES (DEFAULT, 'БАНАНЫ', 11990);
+
+INSERT INTO public.product (id, name, price)
+VALUES (DEFAULT, 'ШОКОЛАД ПОБЕДА ГОРЬКИЙ БЕЗ САХАРА', 14050);
+
+INSERT INTO public.product (id, name, price)
+VALUES (DEFAULT, 'КОНФЕТА ДЖАМП ВАН ОРЕХОВО-ФРУКТОВАЯ', 5840);
+
+INSERT INTO public.product (id, name, price)
+VALUES (DEFAULT, 'МОЛОКО МОЛОЧНАЯ СКАЗКА 3.2% 900Г', 6660);
+
+INSERT INTO public.product (id, name, price)
+VALUES (DEFAULT, 'ВАТНЫЕ ДИСКИ ЭДЕЛЬ 120ШТ', 4390);
+
+INSERT INTO public.product (id, name, price)
+VALUES (DEFAULT, 'КРУПА МАРИЯ ПЕРЛОВАЯ ЯЧМЕННАЯ 1С', 4190);
+
+INSERT INTO public.product (id, name, price)
+VALUES (DEFAULT, 'АРАХИС ЖАРЕНЫЙ СОЛЕНЫЙ', 25740);
+
+INSERT INTO public.product (id, name, price)
+VALUES (DEFAULT, 'ОГУРЦЫ ПУПЫРЧАТЫЕ', 26990);
+
+INSERT INTO public.product (id, name, price)
+VALUES (DEFAULT, 'НАПИТОК НЕ МОЛОКО ОВСЯНЫЙ ШОКОЛАДНЫЙ', 12890);
+
+INSERT INTO public.product (id, name, price)
+VALUES (DEFAULT, 'КОНС.МАСЛИНЫ БАРКО ОТБОРНЫЕ', 9690);
+
+INSERT INTO public.product (id, name, price)
+VALUES (DEFAULT, 'ПЕРЕЦ КРАСНЫЙ', 42500);
+
+INSERT INTO public.product (id, name, price)
+VALUES (DEFAULT, 'ТОМАТЫ СЛИВОВИДНЫЕ 450Г', 17990);
+
+INSERT INTO public.product (id, name, price)
+VALUES (DEFAULT, 'ГРИБЫ ШАМПИНЬОНЫ 300Г', 9950);
+
+INSERT INTO public.product (id, name, price)
+VALUES (DEFAULT, 'ЧЕСНОК ИМПОРТ', 39300);
+
