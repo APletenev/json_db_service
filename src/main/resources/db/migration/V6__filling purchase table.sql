2a916c852a255e53bca69846da8501729908e815
Andrey Pletenev <pletenev.andrey@yandex.ru>
2022-08-11 19:29:03 +0700

    Заполнена таблица purchase

---
+++ src/main/resources/db/migration/V6__filling purchase table.sql
@@ -0,0 +1,400 @@
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (4, 15, '2021-10-12');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (3, 20, '2021-03-05');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (2, 1, '2021-08-12');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (10, 15, '2021-05-01');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (4, 20, '2021-05-24');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (2, 1, '2021-06-05');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (11, 17, '2021-11-07');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (1, 13, '2021-12-20');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (1, 6, '2021-07-15');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (8, 12, '2021-09-16');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (8, 1, '2021-08-30');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (8, 9, '2021-12-16');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (7, 13, '2021-06-08');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (6, 21, '2021-11-15');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (1, 5, '2021-05-05');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (11, 11, '2021-12-12');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (10, 12, '2021-05-22');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (9, 6, '2021-12-29');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (6, 14, '2021-02-02');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (8, 1, '2021-10-22');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (8, 8, '2021-12-17');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (6, 11, '2021-01-20');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (8, 8, '2021-09-16');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (2, 13, '2021-05-24');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (2, 1, '2021-11-04');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (3, 3, '2021-02-26');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (8, 1, '2021-06-13');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (8, 21, '2021-09-28');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (4, 4, '2021-11-25');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (3, 14, '2021-07-14');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (11, 7, '2021-05-24');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (5, 10, '2021-03-26');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (10, 10, '2021-10-06');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (3, 3, '2021-12-19');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (6, 17, '2021-05-31');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (9, 1, '2021-04-06');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (5, 3, '2021-12-26');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (8, 18, '2021-12-22');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (6, 5, '2021-06-14');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (3, 15, '2021-07-31');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (1, 9, '2021-11-24');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (7, 5, '2021-06-05');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (2, 3, '2021-07-30');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (2, 6, '2021-12-02');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (4, 7, '2021-04-29');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (1, 7, '2021-08-30');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (2, 14, '2021-03-12');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (10, 6, '2021-01-04');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (8, 15, '2021-01-03');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (6, 17, '2021-11-25');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (5, 19, '2021-06-23');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (6, 2, '2021-03-30');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (5, 11, '2021-08-08');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (1, 14, '2021-02-26');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (6, 2, '2021-10-11');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (3, 16, '2021-05-29');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (10, 8, '2021-07-12');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (7, 11, '2021-07-11');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (5, 17, '2021-03-25');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (7, 15, '2021-09-16');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (4, 11, '2021-12-25');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (4, 1, '2021-03-31');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (2, 14, '2021-06-01');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (4, 12, '2021-11-20');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (2, 15, '2021-03-13');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (8, 12, '2021-09-05');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (9, 6, '2021-04-10');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (7, 14, '2021-10-26');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (11, 15, '2021-11-27');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (1, 7, '2021-07-16');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (3, 12, '2021-01-13');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (1, 18, '2021-05-10');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (5, 19, '2021-01-27');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (5, 5, '2021-02-13');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (8, 12, '2021-06-06');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (7, 12, '2021-05-29');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (2, 12, '2021-10-28');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (2, 17, '2021-12-03');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (2, 14, '2021-07-04');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (2, 14, '2021-08-15');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (1, 11, '2021-09-14');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (7, 6, '2021-06-11');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (3, 3, '2021-11-22');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (10, 18, '2021-07-15');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (8, 18, '2021-09-15');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (8, 3, '2021-04-14');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (10, 11, '2021-07-15');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (4, 1, '2021-10-24');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (4, 4, '2021-09-28');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (8, 13, '2021-08-24');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (4, 16, '2021-08-14');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (8, 2, '2021-03-17');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (11, 18, '2021-10-11');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (3, 13, '2021-04-20');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (5, 13, '2021-05-31');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (9, 9, '2021-02-07');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (1, 6, '2021-03-11');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (10, 7, '2021-05-21');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (6, 8, '2021-05-18');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (11, 17, '2021-01-25');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (11, 18, '2021-10-19');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (7, 9, '2021-07-13');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (4, 9, '2021-10-13');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (10, 16, '2021-01-10');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (11, 5, '2021-09-16');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (10, 16, '2021-03-21');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (4, 16, '2021-02-14');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (3, 9, '2021-03-26');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (7, 4, '2021-08-31');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (11, 9, '2021-02-01');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (1, 19, '2021-01-20');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (3, 6, '2021-05-11');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (10, 15, '2021-05-18');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (7, 15, '2021-12-31');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (5, 5, '2021-09-20');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (1, 14, '2021-12-16');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (8, 14, '2021-05-28');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (10, 12, '2021-08-22');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (11, 15, '2021-03-31');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (10, 2, '2021-12-02');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (5, 15, '2021-08-07');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (11, 17, '2021-05-14');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (7, 19, '2021-05-19');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (1, 7, '2021-06-05');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (11, 2, '2021-08-26');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (9, 1, '2021-10-15');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (9, 15, '2021-01-17');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (1, 6, '2021-01-27');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (4, 13, '2021-06-03');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (3, 15, '2021-08-10');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (10, 12, '2021-04-12');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (2, 15, '2021-03-09');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (4, 10, '2021-06-02');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (5, 2, '2021-12-08');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (7, 14, '2021-01-12');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (11, 15, '2021-08-21');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (6, 10, '2021-11-08');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (9, 21, '2021-01-11');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (6, 21, '2021-07-18');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (8, 1, '2021-06-12');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (10, 11, '2021-08-19');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (4, 1, '2021-01-05');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (4, 15, '2021-09-03');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (2, 4, '2021-03-15');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (6, 4, '2021-10-20');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (10, 14, '2021-02-13');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (9, 9, '2021-08-04');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (3, 6, '2021-06-23');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (2, 17, '2021-01-21');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (4, 16, '2021-07-24');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (2, 17, '2021-10-06');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (5, 4, '2021-11-04');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (7, 12, '2021-04-17');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (6, 20, '2021-03-20');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (9, 20, '2021-06-28');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (8, 8, '2021-01-06');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (3, 19, '2021-03-13');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (1, 4, '2021-10-27');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (7, 7, '2021-05-07');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (8, 15, '2021-05-02');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (5, 16, '2021-10-02');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (11, 11, '2021-09-25');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (5, 12, '2021-03-09');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (7, 18, '2021-01-08');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (10, 12, '2021-11-06');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (2, 21, '2021-05-03');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (9, 3, '2021-03-01');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (9, 11, '2021-03-03');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (4, 16, '2021-03-16');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (2, 21, '2021-02-18');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (8, 11, '2021-04-07');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (1, 19, '2021-07-26');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (3, 16, '2021-02-16');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (10, 5, '2021-01-19');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (8, 16, '2021-05-15');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (11, 17, '2021-09-30');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (2, 2, '2021-06-21');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (1, 11, '2021-11-10');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (7, 16, '2021-07-16');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (7, 13, '2021-08-03');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (6, 15, '2021-06-14');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (2, 16, '2021-05-25');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (3, 17, '2021-06-21');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (8, 4, '2021-12-20');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (7, 18, '2021-07-06');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (9, 1, '2021-08-31');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (9, 3, '2021-03-18');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (9, 15, '2021-06-01');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (7, 13, '2021-05-25');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (5, 12, '2021-09-09');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (2, 20, '2021-03-23');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (2, 21, '2021-04-15');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (11, 8, '2021-09-08');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (3, 9, '2021-07-18');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (11, 5, '2021-06-20');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (3, 14, '2021-02-20');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (11, 1, '2021-09-21');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (1, 4, '2021-10-28');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (11, 10, '2021-07-18');
+
+INSERT INTO public.purchase (customer_id, product_id, date) VALUES (5, 21, '2021-02-02');
+
