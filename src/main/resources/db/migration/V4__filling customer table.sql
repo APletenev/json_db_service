48150fa63d9aeee773e15842d18185d5b37ffdcd
Andrey Pletenev <pletenev.andrey@yandex.ru>
2022-08-11 16:57:13 +0700

    Заполнена таблица customer

---
+++ src/main/resources/db/migration/V4__filling customer table.sql
@@ -0,0 +1,32 @@
+INSERT INTO public.customer (id, last_name, first_name)
+VALUES (DEFAULT, 'Иванов', 'Антон');
+
+INSERT INTO public.customer (id, last_name, first_name)
+VALUES (DEFAULT, 'Петров', 'Валентин');
+
+INSERT INTO public.customer (id, last_name, first_name)
+VALUES (DEFAULT, 'Сидоров', 'Николай');
+
+INSERT INTO public.customer (id, last_name, first_name)
+VALUES (DEFAULT, 'Иванов', 'Николай');
+
+INSERT INTO public.customer (id, last_name, first_name)
+VALUES (DEFAULT, 'Орлова', 'Ольга');
+
+INSERT INTO public.customer (id, last_name, first_name)
+VALUES (DEFAULT, 'Ласточкин', 'Артем');
+
+INSERT INTO public.customer (id, last_name, first_name)
+VALUES (DEFAULT, 'Пушкин', 'Александр');
+
+INSERT INTO public.customer (id, last_name, first_name)
+VALUES (DEFAULT, 'Горбачёв', 'Михаил');
+
+INSERT INTO public.customer (id, last_name, first_name)
+VALUES (DEFAULT, 'Киркоров', 'Филипп');
+
+INSERT INTO public.customer (id, last_name, first_name)
+VALUES (DEFAULT, 'Пугачёва', 'Алла');
+
+INSERT INTO public.customer (id, last_name, first_name)
+VALUES (DEFAULT, 'Кукушкина', 'Серафима');
